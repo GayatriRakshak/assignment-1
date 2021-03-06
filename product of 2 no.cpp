bf2d631b885d2287adf0e737a1abecc58b5f8ad5
MICKEY935974 <69594578+MICKEY935974@users.noreply.github.com>
2021-03-06 17:45:24 +0530

    Add files via upload

---
+++ product of 2 no.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float a,b,mul;
+	cout<<"Enter first  number =";
+	cin>>a;
+	cout<<"Enter second number =";
+	cin>>b;
+	
+	mul = a*b;
+	cout <<"Product of two no is =" << mul ;
+	return 0;
+}
